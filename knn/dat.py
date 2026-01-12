c81b167d69b2f631a086cca7e9a6b6ee9c5280cc
Semin_Rwai <hwangchei901@gmail.com>
2026-01-11 22:32:01 -0500

    wau

--- knn/dat.py
+++ knn/dat.py
@@ -4,7 +4,7 @@ import scipy.stats as ss
 def generate_synth_data(n=50):
     '''
     Create 2 sets of points from bivariate normal distributions.
-    '''
+    ''' 
     points = np.concatenate((ss.norm(0,1).rvs((n,2)),ss.norm(1,1).rvs((n,2))), axis =0)
     outcomes = np.concatenate((np.repeat(0,n), np.repeat(1,n)))
     return (points, outcomes)
